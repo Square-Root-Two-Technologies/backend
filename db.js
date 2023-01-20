c3d8e34a18a223d87356d238045585c8af189218
root_2 <yolotanvir@gmail.com>
2023-01-20 16:24:02 +0530

    strict query fix

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ dotenv.config({ path: "./config.env" });
 
 const mongoURI = process.env.DATABASE;
 //"mongodb://localhost:27017/iNotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
-
+mongoose.set("strictQuery", false);
 const connectToMongo = () => {
   mongoose
     .connect(mongoURI, () => {
