173f33a48ee6ce11435c59d5edf9342e418a88e2
root_2 <yolotanvir@gmail.com>
2023-02-25 03:02:39 +0530

    new changes

--- models/User.js
+++ models/User.js
@@ -1,24 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-    name:{
-        type: String,
-        required: true
-    },
-    email:{
-        type: String,
-        required: true,
-        unique: true
-    },
-    password:{
-        type: String,
-        required: true
-    },
-    date:{
-        type: Date,
-        default: Date.now
-    },
-  });
-  const User = mongoose.model('user', UserSchema);
-  module.exports = User;
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  country: {
+    type: String,
+    required: false,
+  },
+  city: {
+    type: String,
+    required: false,
+  },
+  about: {
+    type: String,
+    required: false,
+    default: "about is empty",
+  },
+});
+const User = mongoose.model("user", UserSchema);
+module.exports = User;
