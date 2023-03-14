e0deca14ad6b799a4d4902b2feb78ed27db865cf
root_2 <yolotanvir@gmail.com>
2023-03-14 16:46:19 +0530

    filter by type for blogs fix

--- db.js
+++ db.js
@@ -3,7 +3,6 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
 const mongoURI = process.env.DATABASE;
-//"mongodb://localhost:27017/iNotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
 mongoose.set("strictQuery", false);
 const connectToMongo = () => {
   mongoose
